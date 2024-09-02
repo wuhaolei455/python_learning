842105c20aa514d99a325b800f0d567e11f18353
wuhaolei <nell53052@gmail.com>
2024-09-02 23:55:29 +0800

    MOD: 添加注释

--- cat.py
+++ cat.py
@@ -1,3 +1,24 @@
+"""
+The 'cat' Program Implemented in Python 3
+
+The Unix 'cat' utility reads the contents
+of file(s) specified through stdin and 'conCATenates'
+into stdout. If it is run without any filename(s) given,
+then the program reads from standard input itself,
+which means it simply copies stdin to stdout.
+
+It is fairly easy to implement such a program
+in Python, and as a result countless examples
+exist online. This particular implementation
+focuses on the basic functionality of the cat
+utility. Compatible with Python 3.6 or higher.
+
+Syntax:
+python3 cat.py [filename1] [filename2] etc...
+Separate filenames with spaces.
+
+wuhaolei - 09/02/2024
+"""
 import sys
 
 def with_files(files):
